321fa38142c9d92f2e64eb33c58b91f52b988e07
PannaAryan <panna.codebase@gmail.com>
2025-08-28 02:02:37 +0600

    behance field added properly

--- portfolio/models.py
+++ portfolio/models.py
@@ -14,6 +14,7 @@ class UserProfile(models.Model):
     github_url = models.URLField(blank=True)
     linkedin_url = models.URLField(blank=True)
     twitter_url = models.URLField(blank=True)
+    behance_url = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
